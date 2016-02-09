fafbbd1987861944f98813cf82e7f5dbb25927d1
Russel Van Tuyl <Russel.VanTuyl@gmail.com>
2016-02-09 16:35:11 -0500

    Fixed retest statistics calculations of remaining vulnerable host totals

--- Guinevere.py
+++ Guinevere.py
@@ -632,7 +632,7 @@ def retest():
     banner()
     print "Retrieving available assessments..."
 
-    #Collect data from the original assessment
+    # Collect data from the original assessment
     original_assessment = get_assessment("the original assessment")
     banner()
     original_crosstable = get_crosstable(original_assessment)
@@ -641,7 +641,7 @@ def retest():
     print "["+note+"]Gathering original assessment vulnerability dataset..."
     original_vuln = get_vulns(original_vID, original_assessment, original_crosstable)
 
-    #Collect data from the retest
+    # Collect data from the retest
     retest_assessment = get_assessment("the retest assessment")
     banner()
     retest_crosstable = get_crosstable(retest_assessment)
@@ -650,11 +650,11 @@ def retest():
     print "["+note+"]Gathering retest vulnerability dataset..."
     retest_vuln = get_vulns(retest_vID, retest_assessment, retest_crosstable)
 
-    #Create the report stub
+    # Create the report stub
     retest_report = docx.Document()
     retest_report.add_heading(original_assessment+' Retest Results')
 
-    retest = {} #Dictionary to hold retest data
+    retest = {} # Dictionary to hold retest data
 
     for i in original_vuln:
         if original_vuln[i]['vuln_rating'] is not None and original_vuln[i]['vuln_rating'] is not "Informational":
@@ -685,7 +685,7 @@ def retest():
                 print "\t["+note+"]" + original_vuln[i]['vuln_title'] + " - Remediated"
                 retest[i].update({'status': 'Remediated'})
 
-    #Build Status Table
+    # Build Status Table
     retest_report.add_heading('Vulnerability Status')
     status_table = retest_report.add_table(rows=1, cols=3)
     status_table.style = 'Medium Grid 1 Accent 1'
@@ -694,7 +694,7 @@ def retest():
     hdr_cells[1].text = 'Vulnerability'
     hdr_cells[2].text = 'Status'
 
-    #Add Critical first
+    # Add Critical first
     for i in retest:
         if retest[i]['vuln_rating'] is 'Critical':
             row_cells = status_table.add_row().cells
@@ -702,7 +702,7 @@ def retest():
             row_cells[1].text = retest[i]['vuln_title']
             row_cells[2].text = retest[i]['status']
 
-    #Add High second
+    # Add High second
     for i in retest:
         if retest[i]['vuln_rating'] is 'High':
             row_cells = status_table.add_row().cells
@@ -710,7 +710,7 @@ def retest():
             row_cells[1].text = retest[i]['vuln_title']
             row_cells[2].text = retest[i]['status']
 
-    #Add Medium third
+    # Add Medium third
     for i in retest:
         if retest[i]['vuln_rating'] is 'Medium':
             row_cells = status_table.add_row().cells
@@ -718,7 +718,7 @@ def retest():
             row_cells[1].text = retest[i]['vuln_title']
             row_cells[2].text = retest[i]['status']
 
-    #Add Low last
+    # Add Low last
     for i in retest:
         if retest[i]['vuln_rating'] is 'Low':
             row_cells = status_table.add_row().cells
@@ -726,7 +726,7 @@ def retest():
             row_cells[1].text = retest[i]['vuln_title']
             row_cells[2].text = retest[i]['status']
 
-    #Build Still Vulnerable Hosts Table
+    # Build Still Vulnerable Hosts Table
     retest_report.add_heading('Hosts That Are Still Vulnerable')
     vulnerable_table = retest_report.add_table(rows=1, cols=2)
     vulnerable_table.style = 'Medium Grid 1 Accent 1'
@@ -744,9 +744,9 @@ def retest():
                 hosts.append(h[0])
             row_cells[1].text = ((str(ip_sort(hosts)).replace("'", "")).lstrip("[")).rstrip("]")
 
-    #Build stats table
-    o_total_c = 0   #Original Total Critical
-    r_total_c = 0   #Retest Total Critical
+    # Build stats table
+    o_total_c = 0   # Original Total Critical
+    r_total_c = 0   # Retest Total Critical
     o_total_h = 0
     r_total_h = 0
     o_total_m = 0
@@ -754,30 +754,30 @@ def retest():
     o_total_l = 0
     r_total_l = 0
     for i in retest:
-        #Critical Vulnerabilities
+        # Critical Vulnerabilities
         if retest[i]['vuln_rating'] is 'Critical':
             o_total_c += retest[i]['total_orig']
-            if 'total_retest' in retest[i]:
-                r_total_c += retest[i]['total_retest']
-        #High Vulnerabilities
+            if 'v_hosts' in retest[i]:
+                r_total_c += len(retest[i]['v_hosts'])
+        # High Vulnerabilities
         if retest[i]['vuln_rating'] is 'High':
             o_total_h += retest[i]['total_orig']
-            if 'total_retest' in retest[i]:
-                r_total_h += retest[i]['total_retest']
-        #Medium Vulnerabilities
+            if 'v_hosts' in retest[i]:
+                r_total_h += len(retest[i]['v_hosts'])
+        # Medium Vulnerabilities
         if retest[i]['vuln_rating'] is 'Medium':
             o_total_m += retest[i]['total_orig']
-            if 'total_retest' in retest[i]:
-                r_total_m += retest[i]['total_retest']
-        #Low Vulnerabilities
+            if 'v_hosts' in retest[i]:
+                r_total_m += len(retest[i]['v_hosts'])
+        # Low Vulnerabilities
         if retest[i]['vuln_rating'] is 'Low':
             o_total_l += retest[i]['total_orig']
-            if 'total_retest' in retest[i]:
-                r_total_l += retest[i]['total_retest']
+            if 'v_hosts' in retest[i]:
+                r_total_l += len(retest[i]['v_hosts'])
 
     s = "The original security assessment identified (" + str(o_total_c) + ") critical-severity, (" + str(o_total_h) + ") high-severity, (" + str(o_total_m) + ") medium-severity, and (" + str(o_total_l) + ") low-severity vulnerabilities."
 
-    #Setup Table
+    # Setup Table
     retest_report.add_heading('Retest Statistics')
     retest_report.add_paragraph(s)
     stats_table = retest_report.add_table(rows=1, cols=5)
@@ -788,7 +788,7 @@ def retest():
     hdr_cells[2].text = 'High'
     hdr_cells[3].text = 'Medium'
     hdr_cells[4].text = 'Low'
-    #Original Assessment Numbers
+    # Original Assessment Numbers
     row_cells = stats_table.add_row().cells
     row_cells[0].text = 'Original'
     row_cells[1].text = str(o_total_c)
@@ -796,7 +796,7 @@ def retest():
     row_cells[3].text = str(o_total_m)
     row_cells[4].text = str(o_total_l)
 
-    #Retest Assessment Numbers
+    # Retest Assessment Numbers
     row_cells = stats_table.add_row().cells
     row_cells[0].text = 'Retest'
     row_cells[1].text = str(r_total_c)
